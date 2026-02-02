ccd6f238c050b57d6ed28facc616693342c16451
Aditya Chaurasiya <adityachaurasiya57527@gmail.com>
2026-02-02 18:47:43 +0530

    Bump version to 1.2.6 for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name="text-summarizer-aweebtaku",
-    version="1.2.5",
+    version="1.2.6",
     author="Aditya Chaurasiya",
     author_email="adityachaurasiya57527@gmail.com",
     description="A text summarization tool using GloVe embeddings and PageRank algorithm",
